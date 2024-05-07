c814652aafbaab888c3d74d44cfb1cbff5b961f3
trongnghia <trongnghia.25602@gmail.com>
2024-05-07 16:57:20 +0700

    Update server done

--- app.py
+++ app.py
@@ -6,11 +6,24 @@ from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
 from textSummarizer.pipeline.prediction import PredictionPipeline
+from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware
+
+class DataRequest(BaseModel):
+    text: str
+    levelValue: int
 
 text:str = "What is Text Summarization?"
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["authentication"])
 async def index():
@@ -41,5 +54,18 @@ async def predict_route(text):
         raise e
     
 
+@app.post("/process")
+async def process_route(data: DataRequest):
+    try:
+        obj = PredictionPipeline()
+        result = obj.predict(data.text, data.levelValue)
+        return result
+        
+        # print(data)
+        # return {"result": "ok"}
+    except Exception as e:
+        raise e
+
+
 if __name__=="__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="localhost", port=8080)
--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -3,15 +3,16 @@ from transformers import AutoTokenizer
 from transformers import pipeline
 
 
-class PredictionPipeline:
-    def __init__(self):
+class PredictionPipeline():
+    def __init__(self, levelValue = 128):
+        self.levelValue = levelValue
         self.config = ConfigurationManager().get_model_evaluation_config()
 
 
     
-    def predict(self,text):
+    def predict(self,text, levelValue):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+        gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": levelValue}
 
         pipe = pipeline("summarization", model=self.config.model_path,tokenizer=tokenizer)
 
